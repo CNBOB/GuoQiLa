2d96afeb3c237e9433e9b31f5d0ed27d7f85054e
BOB <zhubo0228@163.com>
2016-07-14 14:46:47 +0800

    添加menu

--- app/src/main/java/com/namilili/guoqila/MainActivity.java
+++ app/src/main/java/com/namilili/guoqila/MainActivity.java
@@ -3,12 +3,10 @@ package com.namilili.guoqila;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,4 +29,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_mainactivity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
