5137064e1398d5adc6f82091dbc5089eaeaf5dc7
BOB <zhubo0228@163.com>
2016-07-14 14:32:22 +0800

    添加menu

--- app/src/main/java/com/namilili/guoqila/MainActivity.java
+++ app/src/main/java/com/namilili/guoqila/MainActivity.java
@@ -1,8 +1,10 @@
 package com.namilili.guoqila;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -21,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,"test",Toast.LENGTH_LONG).show();
+                startActivity(new Intent(MainActivity.this,AddItemActivity.class));
+//                Toast.makeText(MainActivity.this,"test",Toast.LENGTH_LONG).show();
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
             }
