72ac2bca95f25470dd40b98b0c32c32c84949466
BOB <zhubo0228@163.com>
2016-08-10 17:24:04 +0800

    备注

--- app/src/main/java/com/namilili/guoqila/AddItemActivity.java
+++ app/src/main/java/com/namilili/guoqila/AddItemActivity.java
@@ -1,13 +1,92 @@
 package com.namilili.guoqila;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Toast;
 
-public class AddItemActivity extends AppCompatActivity {
+import java.io.IOException;
 
+public class AddItemActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    private SurfaceView svItemAddImage;
+    private static Context context = null;
+    private SurfaceHolder surfaceholder;
+    private Camera camera = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+        context = this;
+        svItemAddImage = (SurfaceView) this.findViewById(R.id.itemImage);
+        surfaceholder = svItemAddImage.getHolder();
+        surfaceholder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        surfaceholder.addCallback();
+        svItemAddImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Toast.makeText(getApplicationContext(),"test",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        System.out.println("surfacecreated");
+        //获取camera对象
+        camera = Camera.open();
+        try {
+            //设置预览监听
+            camera.setPreviewDisplay(holder);
+            Camera.Parameters parameters = camera.getParameters();
+
+            if (this.getResources().getConfiguration().orientation
+                    != Configuration.ORIENTATION_LANDSCAPE) {
+                parameters.set("orientation", "portrait");
+                camera.setDisplayOrientation(90);
+                parameters.setRotation(90);
+            } else {
+                parameters.set("orientation", "landscape");
+                camera.setDisplayOrientation(0);
+                parameters.setRotation(0);
+            }
+            camera.setParameters(parameters);
+            //启动摄像头预览
+            camera.startPreview();
+            System.out.println("camera.startpreview");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            camera.release();
+            System.out.println("camera.release");
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        System.out.println("surfaceDestroyed");
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        System.out.println("surfacechanged");
     }
 }
