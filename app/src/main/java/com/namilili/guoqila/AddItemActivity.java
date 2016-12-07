363cc363dc0214b9d5c3dcc48376c8c7aa1f8f06
BOB <zhubo0228@163.com>
2016-12-07 18:01:35 +0800

    备注

--- app/src/main/java/com/namilili/guoqila/AddItemActivity.java
+++ app/src/main/java/com/namilili/guoqila/AddItemActivity.java
@@ -1,5 +1,6 @@
 package com.namilili.guoqila;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
@@ -9,33 +10,61 @@ import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
 
-public class AddItemActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+public class AddItemActivity extends AppCompatActivity{
 
-    private SurfaceView svItemAddImage;
+//    private SurfaceView svItemAddImage;
     private static Context context = null;
-    private SurfaceHolder surfaceholder;
-    private Camera camera = null;
+ //   private SurfaceHolder surfaceholder;
+ //   private Camera camera = null;
+//    private DatePicker myExpDatePicker;
+    private Calendar calendar;
+    private AutoCompleteTextView myExpDate;
+    private AutoCompleteTextView myItemConent;
+
+    private int year;
+    private int month;
+    private int day;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
         context = this;
-        svItemAddImage = (SurfaceView) this.findViewById(R.id.itemImage);
-        surfaceholder = svItemAddImage.getHolder();
-        surfaceholder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        surfaceholder.addCallback();
-        svItemAddImage.setOnClickListener(new View.OnClickListener() {
+
+        // 获取日历对象
+        calendar = Calendar.getInstance();
+        // 获取当前对应的年、月、日的信息
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        myExpDate = (AutoCompleteTextView) findViewById(R.id.tvExpDate);
+        myItemConent = (AutoCompleteTextView) findViewById(R.id.itemContent);
+        myExpDate.setOnFocusChangeListener(new View.OnFocusChangeListener(){
             @Override
-            public void onClick(View view) {
-
-
-                Toast.makeText(getApplicationContext(),"test",Toast.LENGTH_LONG).show();
+            public void onFocusChange(View view, boolean b) {
+                if (b) {
+                    // 初始化DatePickerDialog
+                    new DatePickerDialog(AddItemActivity.this, new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                            myExpDate.setText(year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
+                            month = monthOfYear;
+                            day = dayOfMonth;
+                            myItemConent.requestFocus();
+                        }
+                    }, year, month, day).show();
+                }
             }
         });
+
     }
 
     @Override
@@ -45,48 +74,10 @@ public class AddItemActivity extends AppCompatActivity implements SurfaceHolder.
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder) {
-        System.out.println("surfacecreated");
-        //获取camera对象
-        camera = Camera.open();
-        try {
-            //设置预览监听
-            camera.setPreviewDisplay(holder);
-            Camera.Parameters parameters = camera.getParameters();
-
-            if (this.getResources().getConfiguration().orientation
-                    != Configuration.ORIENTATION_LANDSCAPE) {
-                parameters.set("orientation", "portrait");
-                camera.setDisplayOrientation(90);
-                parameters.setRotation(90);
-            } else {
-                parameters.set("orientation", "landscape");
-                camera.setDisplayOrientation(0);
-                parameters.setRotation(0);
-            }
-            camera.setParameters(parameters);
-            //启动摄像头预览
-            camera.startPreview();
-            System.out.println("camera.startpreview");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            camera.release();
-            System.out.println("camera.release");
-        }
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-        System.out.println("surfaceDestroyed");
-        if (camera != null) {
-            camera.stopPreview();
-            camera.release();
-        }
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-        System.out.println("surfacechanged");
+    public boolean onSupportNavigateUp() {
+        Toast.makeText(this,"test",Toast.LENGTH_LONG).show();
+//        finish();
+//        return super.onSupportNavigateUp();
+        return true;
     }
 }
